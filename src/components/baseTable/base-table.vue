1dba3dbd258ad461d6a47fa4bd27d5ffb960c856
v_yymo <v_yymo@tencent.com>
2022-06-14 22:34:41 +0800

    first-commit

---
+++ src/components/baseTable/base-table.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="base-table">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BaseElTable',
+    props: {
+
+    }
+}
+</script>
---
+++ src/views/table/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="table">
+
+    </div>
+</template>
+
+<script>
+import LbTable from '@/components/baseTable/base-table.vue'
+
+export default {
+    
+}
+</script>
